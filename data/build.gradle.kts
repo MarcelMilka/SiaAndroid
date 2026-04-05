5477fff552beb22c3543030516d7f9c2ce183c64
Marcello <marcello.sw.dev@gmail.com>
2026-04-05 23:28:25 +0200

    Configure the `:data` module and enable cleartext traffic for debug builds
    - Enable `android:usesCleartextTraffic` in a new debug `AndroidManifest.xml` to allow HTTP communication during development.
    - Update `data/build.gradle.kts` to enable `buildConfig` and define `API_BASE_URL` fields for debug and release builds, sourced from local properties.
    - Add Hilt, Retrofit, OkHttp, and Gson dependencies to the `:data` module.
    - Add testing dependencies including MockK, MockWebServer, and Coroutines test utilities.
    - Include project dependencies for `:common` and `:auth` in the `:data` module.

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,10 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -17,13 +21,30 @@ android {
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = false
+
+            val apiBaseUrlDebug = gradleLocalProperties(rootDir, providers)
+                .getProperty("API_BASE_URL_DEBUG") ?: "\"http://example.com\""
+
+            buildConfigField("String", "API_BASE_URL", apiBaseUrlDebug)
+        }
         release {
             isMinifyEnabled = false
+
+            val apiBaseUrlRelease = gradleLocalProperties(rootDir, providers)
+                .getProperty("API_BASE_URL_RELEASE") ?: "\"https://example.com\""
+
+            buildConfigField("String", "API_BASE_URL", apiBaseUrlRelease)
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
+    }
+    buildFeatures {
+        buildConfig = true
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -41,4 +62,30 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //  Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+
+    // Retrofit
+    implementation(libs.retrofit)
+
+    // OkHttp
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+    testImplementation(libs.mockwebserver)
+
+    // Gson converter
+    implementation(libs.converter.gson)
+
+    // MockK
+    testImplementation(libs.mockk)
+    testImplementation(libs.mockk.android)
+    testImplementation(libs.mockk.agent)
+
+    // Coroutines test
+    testImplementation(libs.kotlinx.coroutines.test)
+
+    implementation(project(":common"))
+    implementation(project(":auth"))
 }
